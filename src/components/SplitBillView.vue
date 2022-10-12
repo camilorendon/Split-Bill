a5b708ddd4ca299d152cf5dbc93a164ce656d4ce
camilorendon <juancamilo.g.0512@gmail.com>
2022-10-12 13:16:31 -0500

    update

--- src/components/SplitBillView.vue
+++ src/components/SplitBillView.vue
@@ -1,14 +1,19 @@
 <script setup>
+import InputForm from './inputForm.vue';
+import PeopleView from './PeopleView.vue';
 
 
 </script>
 
 <template>
 <div class="container">
-    
+    <InputForm/>
+    <PeopleView/>
 </div>
 </template>
 
 <style scoped>
-
+.container{
+    display: flex;
+}
 </style>
---
+++ src/components/inputForm.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+import {store, calculate} from "../store/store"
+const total =ref(0);
+const tip =ref(0);
+const people =ref(0);
+
+function handleSubmit(){
+    store.params.total =total;
+    store.params.tip = tip;
+    store.params.people= people;
+    calculate();
+}
+
+</script>
+
+<template>
+   <form action="" @submit.prevent="handleSubmit">
+    <div class="form-container">
+        <div>
+            <lavel for="total">Total</lavel>
+            <input type="numer" required v-model="total" />
+        </div>
+
+        <div>
+            <lavel for="tip">tip %</lavel>
+            <input type="numer" required v-model="tip" />
+        </div>
+
+        <div>
+            <lavel for="people">people</lavel>
+            <input type="numer" required v-model="people" />
+        </div>
+        <div>
+            <input type="submit" value="calculate"/>
+        </div>
+
+    </div>
+   </form>
+</template>
+
+<style scoped></style>
